23c83586d813e6a4dc905e5000b93b019759f36d
Neil Graham <lerc@squishy>
2014-07-16 10:09:43 +1200

    connection closing fixes and infomessage

--- websession.js
+++ websession.js
@@ -9,6 +9,13 @@ process.env.WEBSESSION=localSocketPath;
 
 var connectCounter = 1;
 var connections = [];
+function getNewId(){
+	return connectCounter++;
+}
+
+var opcode_open_notify=0;
+var opcode_close_notify=1;
+var opcode_data=2;
 
 function closeAllConnections() {
 	connections.forEach(function(e,i,a){if (e) {e.end(); delete a[i];}} );	
@@ -38,11 +45,12 @@ function makeHeader(id,cmd) {
 }
 
 function sendInfoMessage(text) {
-	 var header = makeHeader(0,0);
+	 var header = makeHeader(0,99);
 	 var data = new Buffer(text);
 	 header.writeInt32LE(data.length,8);	 
 	 webConnection.sendBytes(Buffer.concat([header,data]));		
 }
+
 var webConnection = new websocket.connection(socket, [], subProtocol, false, config);
 
 webConnection.on("message",function(message)  {
@@ -52,41 +60,58 @@ webConnection.on("message",function(message)  {
 		var opcode = data.readUInt8(4);
 		var length = data.readInt32LE(8);
 		
-		var recipient = connections[id];
+		
+		var recipient = connections[id];	
+		//sendInfoMessage("got message of opcode "+opcode);
 		if (recipient) {
-			recipient.write(data.slice(16));
+			if (opcode==opcode_close_notify) {
+				sendInfoMessage("remote closed connection so we're killing our side");
+				recipient.end(data.slice(16));
+				delete connections[id];
+			} else {
+				recipient.write(data.slice(16));
+			}
 		}
 	}
 });
 
 webConnection.on("close", closeAllConnections);
 
+
 function handleNewConnections(c) {
-	var id = connectCounter++;
+	var id = getNewId();
+	id=Math.floor(Math.random()*10000);
+  c.id=id;
 	connections[id] = c;
-	
+	sendInfoMessage("connections :"+(connections.length));
+	console.log("\n\nnew connection arrived");
+	console.log(connections);
+
 	function sendCloseNotification() {
-		var header = makeHeader(id,1); //1 for close Notification
+		var header = makeHeader(id,opcode_close_notify); 
 		webConnection.sendBytes(header);
 	}
 	
 	function sendData(data) {
-		var header = makeHeader(id,2); //2 for bytes data send
+		var header = makeHeader(c.id,opcode_data); 
 		header.writeInt32LE(data.length,8);
 		webConnection.sendBytes(Buffer.concat([header,data]));		
 	}
+	
 	c.on("data",function (data) {
 		sendData(data);
 	});
+	
 	c.on("close", function () {
 		sendCloseNotification();
 		delete connections[id];		
 	});
 	 
-	webConnection.sendBytes(makeHeader(id,0)); //1 for open Notification
+	webConnection.sendBytes(makeHeader(id,opcode_open_notify)); 
 }
 
-var server = net.createServer(handleNewConnections);
+var server = net.createServer();
+server.on("connection",handleNewConnections)
 server.listen(localSocketPath);
 server.unref();
 
